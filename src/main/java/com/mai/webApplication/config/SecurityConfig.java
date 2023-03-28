26498da8832311b3f5434c36d530ff3ea435fde2
Nikolaevskiy Alexander <nikolaybarmalay@gmail.com>
2023-03-28 19:07:12 +0300

    Create authentication

--- src/main/java/com/mai/webApplication/config/SecurityConfig.java
+++ src/main/java/com/mai/webApplication/config/SecurityConfig.java
@@ -1,24 +1,32 @@
 package com.mai.webApplication.config;
 
-import com.mai.webApplication.security.AuthProviderImpl;
+import com.mai.webApplication.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(UserDetailsServiceImpl userDetailsServiceImpl) {
+        this.userDetailsServiceImpl = userDetailsServiceImpl;
     }
 
 
     //Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsServiceImpl);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder () {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
